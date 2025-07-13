47d4d3d8157df3f6b18c98759cef618a0db9d32a
Alejandro506 <143157137+Alejandro506@users.noreply.github.com>
2025-07-12 22:38:32 -0600

    Tarea realizada

--- myFirstProject/myInterfaces/ILibroRepository.cs
+++ myFirstProject/myInterfaces/ILibroRepository.cs
@@ -5,7 +5,7 @@ namespace myFirstProject.MyInterfaces
 {
     public interface ILibroRepository
     {
-        LibroViewModel GetById(int id);
+        LibroViewModel? GetById(int id);
         IEnumerable<LibroViewModel> GetByFullName(string fullname);
         IEnumerable<LibroViewModel> GetByYearRange(int fromYear, int toYear);
         IEnumerable<LibroViewModel> GetByEditor(string editor);
